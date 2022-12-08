8dfe0049dcbb146c6e062f9d10c16a2fa4d6f608
Jeremie Windows <jeremie.bentolila01@etu.umontpellier.fr>
2022-12-08 22:38:30 +0100

    Jour 8 terminé

--- 2022/jour8/puzzle.py
+++ 2022/jour8/puzzle.py
@@ -8,34 +8,48 @@ try:
     lines = f.readlines()
     for i in range(len(lines)):
         lines[i] = lines[i].replace("\n","")
-    visibles= (len(lines)+len(lines[0])-2)*2
-    print(lines, visibles)
-    for i in range(1,len(lines)-1):
-        for j in range(1,len(lines[i])-1):
-            # pasVisible=True
-            
-            visible=all(int(lines[i][j]) <= int(k) for k in lines[i][:j]) and all( int(lines[i][j]) <= int(k[:j]) for k in lines) and all(int(lines[i][j]) <= int(k) for k in lines[i][j:]) and all( int(lines[i][j]) <= int(k[j:]) for k in lines)
-            # for k in range(len(lines[i])):
-            #     if lines[i][k] < lines[i][j] and k!=j:
-            #         pasVisible=False
-            #         print("ici",lines[i][k], lines[i][j])
-            #         break
-            # if pasVisible:
-            #     for k in range(len(lines)):
-            #         if lines[k][j] < lines[i][j] and k!=i:
-            #             pasVisible=False
-            #             print("ici2",lines[i][k], lines[i][j])
-            #             break
-            if visible:
-                print("lines[",i,"][",j,"] :",lines[i][j])
-                visibles+=1
-    print(visibles)
-            
+    if choix == 1:
+        visibles= (len(lines)+len(lines[0])-2)*2
+        for i in range(1,len(lines)-1):
+            for j in range(1,len(lines[i])-1):
+                gauche=any(int(lines[i][j]) <= int(lines[i][k]) for k in range(j))
+                droite=any(int(lines[i][j]) <= int(lines[i][k]) for k in range(j+1,len(lines[i])))
+                haut=any(int(lines[i][j]) <= int(lines[k][j]) for k in range(i))
+                bas=any(int(lines[i][j]) <= int(lines[k][j]) for k in range(i+1,len(lines)))
+                pasVisible = gauche and droite and haut and bas
+                if not pasVisible:
+                    visibles+=1
+        print(visibles)
+    else:
+        maxi=0
+        for i in range(1,len(lines)-1):
+            for j in range(1,len(lines[i])-1):
+                gauche, droite, haut, bas = 0, 0, 0, 0
+                for k in range(j-1,-1,-1):
+                    gauche+=1
+                    if int(lines[i][j]) <= int(lines[i][k]):
+                        break
+                for k in range(j+1,len(lines[i])):
+                    droite+=1
+                    if int(lines[i][j]) <= int(lines[i][k]):
+                        break
+                for k in range(i-1,-1,-1):
+                    haut+=1
+                    if int(lines[i][j]) <= int(lines[k][j]):
+                        break
+                for k in range(i+1,len(lines)):
+                    bas+=1
+                    if int(lines[i][j]) <= int(lines[k][j]):
+                        break
+                scenicScore=gauche*droite*haut*bas
+                if scenicScore > maxi:
+                    maxi=scenicScore
+        print(maxi)
     ####################################################
 except FileNotFoundError:
     if nom_fich == "i" or nom_fich == "it":
         print(f"Vous n'êtes pas dans le bon répertoire !")
     else:
         print(f"'{nom_fich}' n'est pas un nom de fichier correct (noms attendus: i ou it)")
-# except ValueError:
-#     print(f"'{choix}' n'est pas une valeur correcte (valeurs attendues: 1 ou 2)")
+except ValueError:
+    print(f"'{choix}' n'est pas une valeur correcte (valeurs attendues: 1 ou 2)")
